7053c065fd9ebfe513020540d9563dbde321072f
dal-liu <daniel.albert.liu@gmail.com>
2023-07-16 00:25:33 -0400

    Created new branch

--- server.py
+++ server.py
@@ -1,17 +1,10 @@
 import json
+import utils.db as db
 import utils.openmeteo_api as openmeteo_api
 import utils.strava_api as strava_api
 from flask import Flask, request, render_template
-from pyngrok import ngrok
 
 app = Flask(__name__)
-
-
-if not app.debug:
-    with app.app_context():
-        print('Creating ngrok tunnel...')
-        public_url = ngrok.connect(5000).public_url
-        print(f'Callback URL: {public_url}/webhook')
 
 
 @app.route('/')
@@ -25,11 +18,13 @@ def webhook():
         print(f'Event update received {request.json}')
         if request.json['aspect_type'] == 'create':
             id = request.json['object_id']
+            db.check_access_token()
             name, start_date, start_latlng = strava_api.get_name_dt_and_latlng(id)
             if len(start_latlng) == 2:
                 icon, description = openmeteo_api.get_weather_at_point(start_latlng[0], start_latlng[1], start_date)
                 title = icon + name
                 print(f'Updating activity...')
+                db.check_access_token()
                 strava_api.update_activity(id, description, title)
             else:
                 print('Activity did not contain map, did not update activity')
